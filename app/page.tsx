a836f54da5b3fbb104a37e66ce8cb4fb19e3f63d
Phaxe <Yahia.eid@outlook.com>
2025-03-26 02:36:29 +0200

    push

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-10 h-full mb-10 w-full text-center font-semibold my-1  0">
     
-      <MyComp />ss
+      <MyComp />
       
     </div>
   );
